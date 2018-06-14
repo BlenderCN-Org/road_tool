e3abdd90c4b88f1f53753878edea150b99d5fea2
ranjian0 <karanjaichungwa@gmail.com>
2018-06-14 18:48:02 +0300

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "Road Tools",
+    "author": "Ian Ichung'wa Karanja (ranjian0)",
+    "version": (0, 0, 1),
+    "blender": (2, 79, 0),
+    "location": "View3D > Toolshelf > Road Tools",
+    "description": "Road Creation Tools",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Cynthia"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
